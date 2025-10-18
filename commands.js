0a9fa23f1cebbb6872b04c3ad0382a25e3d07f0d
Ayham <ayhmalqran3@gmail.com>
2025-10-18 11:01:09 +0300

    Delete commands.js

--- commands.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    request(data, victimList, io){
-        let { to, action, data: payload } = JSON.parse(data);
-        if(victimList[to]){
-            io.to(victimList[to]).emit(action, payload);
-        }
-    }
-};
